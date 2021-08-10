9bb4828b1998833f2b9d48d6cce72f29dfb51cb7
Maxim-BC <76106605+Maxim-BC@users.noreply.github.com>
2021-08-11 02:15:45 +0300

    install build

---
+++ build/precache-manifest.5b6a45fe2ec2bae428cb94cebac3ebb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042ddd76b02f79ed2043c702f9c86797",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3e5fb7b093a88c254a8",
+    "url": "/static/css/main.9681f887.chunk.css"
+  },
+  {
+    "revision": "4c815a27adeaa8a33424",
+    "url": "/static/js/2.974f28da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.974f28da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e5fb7b093a88c254a8",
+    "url": "/static/js/main.07ec2e47.chunk.js"
+  },
+  {
+    "revision": "97d614f48baeada74120",
+    "url": "/static/js/runtime-main.47eaace9.js"
+  }
+]);
